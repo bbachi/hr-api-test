c415b268a9e430fc780436e4c2f50cfdba365983
Bhargav Bachina <bhargavbachina@yahoo.com>
2019-10-20 09:29:56 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  
+  constructor(private appService: AppService) {
+
+  }
+
+  data: any;
+
+  ngOnInit() {
+    this.appService.getAPI().subscribe(data => {
+       this.data = data;
+    })
+  }
+
 }
